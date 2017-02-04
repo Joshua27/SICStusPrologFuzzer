1e0697c240359211afa526df2a548646ac9126ae
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-02-04 13:00:25 +0100

    set type constraints for identifier sets when generating constraint problems

--- types/b_extension/prob_ast_identifier.pl
+++ types/b_extension/prob_ast_identifier.pl
@@ -1,7 +1,8 @@
 :- multifile generate/2.
 :- multifile shrink/3.
 
-:- use_module(library(lists),[nth1/4]).
+:- use_module(library(lists),[nth1/4, is_list/1]).
+:- use_module(library(random)).
 
 generate(prob_ast_identifier(Type),b(identifier(Name),Type,[])) :- 
     Type = record(_) , ! , 
@@ -10,17 +11,7 @@ generate(prob_ast_identifier(Type),b(identifier(Name),Type,[])) :-
 generate(prob_ast_identifier(Type),b(identifier(Name),NType,[])) :-  
     generate(atom([size:5,alph]),Name) , 
     inner_type(Type,Inner,Outter) , 
-    ( is_list(Inner)
-    ->  % drop options    
-        length(Outter,L) , 
-        nth1(L,Outter,NewInner,NewOutter) , 
-        surround_type(NewInner,NewOutter,NType)
-    ; compound(Inner)
-    ->  % split inner to get rid of options
-        Inner =.. [NewInner|_] , 
-        NewOutter = Outter , 
-        surround_type(NewInner,NewOutter,NType)
-    ;   NType = Type).
+    generate_id_or_ast_aux2(Type,Inner,Outter,NType).
 
 % either generate an identifier or an expression
 generate(id_or_ast(Type),Value) :-  
@@ -29,24 +20,15 @@ generate(id_or_ast(Type),Value) :-
     % remove options for type of identifier
     % argument Type is like set(integer([small]))
     inner_type(Type,Inner,Outter) ,
-    (is_list(Inner)
-    ->  length(Outter,L) , 
-        nth1(L,Outter,NewInner,NewOutter) , 
-        surround_type(NewInner,NewOutter,NType) 
-    ;   NType = Type) , 
+    generate_id_or_ast_aux2(Type,Inner,Outter,NType) , 
     generate(prob_ast_identifier(NType),Value).
     
 generate(id_or_ast(Type),Value) :- 
     inner_type(Type,Inner,Outter) , 
-    (\+is_list(Inner)
-    ->  NewInner =.. [Inner,[]] , 
-        surround_type(NewInner,Outter,Temp) , 
-        gen_type(Temp,ast,NType) 
-    ;   gen_type(Type,ast,NType)) , 
+    generate_id_or_ast_aux(Type,Inner,Outter,NType) , 
     generate(NType,Value).
 
 % generate constraints for a list of identifier nodes
-
 generate(prob_ast_id_constraints(IDs),ConstraintPred) :- 
     generate(prob_ast_id_constraints(IDs,[]),ConstraintPred).
 
@@ -54,16 +36,18 @@ generate(prob_ast_id_constraints(b(Name,integer,Info),Options),ConstraintPred) :
     generate(prob_ast_set_expr(interval,Options),Interval) , 
     ConstraintPred = b(member(b(Name,integer,Info),Interval),pred,[]).
 
-generate(prob_ast_id_constraints(b(Name,set(Type),Info),_),ConstraintPred) :- 
+generate(prob_ast_id_constraints(b(Name,set(InnerType),Info),_),ConstraintPred) :- 
     random_member(Pred,[equal,less,greater,greater_equal,less_equal]) , 
     generate(prob_ast_integer([between(1,10)]),Integer) , 
-    NewNode =.. [Pred,b(card(b(Name,set(Type),Info)),integer,[]),Integer] , 
-    ConstraintPred = b(NewNode,pred,[]).
+    Identifier = b(Name,set(InnerType),Info) , 
+    NewNode =.. [Pred,b(card(Identifier),integer,[]),Integer] , 
+    get_type_constraint(Identifier,InnerType,TypeConstraint) , 
+    ConstraintPred = b(conjunct(b(NewNode,pred,[]),TypeConstraint),pred,[]).
 
-generate(prob_ast_id_constraints(b(Name,seq(Type),Info),_),ConstraintPred) :- 
+generate(prob_ast_id_constraints(b(Name,seq(InnerType),Info),_),ConstraintPred) :- 
     random_member(Pred,[equal,less,greater,greater_equal,less_equal]) , 
     generate(prob_ast_integer([between(1,10)]),Integer) , 
-    NewNode =.. [Pred,b(size(b(Name,seq(Type),Info)),integer,[]),Integer] , 
+    NewNode =.. [Pred,b(size(b(Name,seq(InnerType),Info)),integer,[]),Integer] , 
     ConstraintPred = b(NewNode,pred,[]).
 
 generate(prob_ast_id_constraints([IDNode],Options),ConstraintPred) :- 
@@ -72,12 +56,53 @@ generate(prob_ast_id_constraints([IDNode],Options),ConstraintPred) :-
 generate(prob_ast_id_constraints([IDNode1,IDNode2|R],Options),ConstraintPred) :- 
     generate(prob_ast_id_constraints(IDNode1,Options),Constraint1) , 
     generate(prob_ast_id_constraints(IDNode2,Options),Constraint2) , 
-    (R \= []
-    ->  generate(prob_ast_id_constraints(R,Options),RConstraints) , 
-        ConstraintPred = b(conjunct(b(conjunct(Constraint1,Constraint2),pred,[]),RConstraints),pred,[])
-    ;   ConstraintPred = b(conjunct(Constraint1,Constraint2),pred,[])).
+    generate_identifier_constraint_aux(Constraint1,Constraint2,R,Options,ConstraintPred).
 % skip boolean, string
 generate(prob_ast_id_constraints(_,_),b(truth,pred,[])).
+
+generate_identifier_constraint_aux(Constraint1,Constraint2,R,Options,ConstraintPred) :- 
+    R \= [] , 
+    generate(prob_ast_id_constraints(R,Options),RConstraints) , 
+    ConstraintPred = b(conjunct(b(conjunct(Constraint1,Constraint2),pred,[]),RConstraints),pred,[]).
+generate_identifier_constraint_aux(Constraint1,Constraint2,[],_,ConstraintPred) :- 
+    ConstraintPred = b(conjunct(Constraint1,Constraint2),pred,[]).
+
+get_type_constraint(Identifier,InnerType,TypeConstraint) :- 
+    get_set_type(InnerType,SetType) , ! , 
+    TypeConstraint = b(subset(Identifier,SetType),pred,[]).
+get_type_constraint(_,_,b(truth,pred,[])).
+
+get_set_type(InnerType,SetType) :- 
+    ground(InnerType) , ! , 
+    get_set_type_aux(_,InnerType,SetType).
+get_set_type(_,SetType) :- 
+    random(0,3,R) , 
+    get_set_type_aux(R,_,SetType).
+get_set_type_aux(0,integer,b(integer_set('INTEGER'),set(integer),[])).
+get_set_type_aux(1,integer,b(integer_set('NATURAL'),set(integer),[])).
+get_set_type_aux(2,integer,b(integer_set('NATURAL1'),set(integer),[])).
+
+generate_id_or_ast_aux(_,InnerType,OutterType,NType) :- 
+    \+is_list(InnerType) , 
+    NewInner =.. [InnerType,[]] , 
+    surround_type(NewInner,OutterType,Temp) , 
+    gen_type(Temp,ast,NType).
+generate_id_or_ast_aux(Type,InnerType,_,NType) :- 
+    is_list(InnerType) , 
+    gen_type(Type,ast,NType).
+
+generate_id_or_ast_aux2(_,InnerType,OutterType,NType) :- 
+    is_list(InnerType) , 
+    length(OutterType,L) , 
+    nth1(L,OutterType,NewInner,NewOutter) , 
+    surround_type(NewInner,NewOutter,NType) .
+generate_id_or_ast_aux2(_,InnerType,OutterType,NType) :- 
+    compound(InnerType) , 
+    % split inner to get rid of options
+    InnerType =.. [NewInner|_] , 
+    surround_type(NewInner,OutterType,NType).
+generate_id_or_ast_aux2(Type,InnerType,_,Type) :- 
+    \+is_list(InnerType).
 
 % don't shrink identifier
 shrink(prob_ast_identifier(_),Value,Value).
