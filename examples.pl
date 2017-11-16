9a06aa7287ffeaea4b2a4d2db494485e891f349a
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-11-16 16:59:44 +0100

    update example

--- examples.pl
+++ examples.pl
@@ -1,10 +1,19 @@
-:- use_module(fuzzing,[fuzz/2,generate/2]).
+:- use_module(fuzzing).
 
+% The predicate to be tested.
 reverse(L,R) :-
     reverse(L,[],R).
 reverse([],Acc,Acc).
-reverse([H|T],Acc,R) :- 
+reverse([H|T],Acc,R) :-
     reverse(T,[H|Acc],R).
-% a simple fuzz test
-run_test :- 
+
+% A simple fuzz test generating random lists of atoms where each atom uses only alphabetic characters.
+run_test :-
     fuzz(reverse/2,list(atom([alph]),[]):var).
+% Fuzz the predicate a specific amount of times.
+run_test2 :-
+    fuzz(reverse/2,100000,list(atom([alph]),[]):var).
+
+% Reproduce a test for a specific seed.
+reproduce_example :-
+    reproduce_test(reverse/2,list(atom([alph]),[]):var,random(27134,9213,17773,425005073)).
